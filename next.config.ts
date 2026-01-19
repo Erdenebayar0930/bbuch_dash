23aef621b956ba3d2fdd30012002a433d337b3b4
Erdenebayar0930 <erdenebayar0930@gmail.com>
2026-01-19 14:17:13 +0800

    w

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
-// next.config.js
 const withPWA = require("next-pwa")({
-  dest: "public", // service worker-ийг public/ дотор гаргана
+  dest: "public",
   register: true,
   skipWaiting: true,
   fallbacks: {
     document: "/_offline.html",
   },
-  disable: process.env.NODE_ENV === "development", // dev-д PWA идэвхгүй
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  webpack(config: { module: { rules: { test: RegExp; issuer: RegExp; use: string[]; }[]; }; }) {
+    if (config.module) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      });
+    }
+    return config;
+  },
 });
